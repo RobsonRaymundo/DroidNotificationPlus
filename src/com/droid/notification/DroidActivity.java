193aaee62c8f98a9bdd66f56834a7ad4c83555b4
Robson Raymundo <robson.raymundo@gmail.com>
2017-08-18 18:24:16 -0300

    atualizando as imagens

--- src/com/droid/notification/DroidActivity.java
+++ src/com/droid/notification/DroidActivity.java
@@ -18,67 +18,54 @@ public class DroidActivity extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
-        try
-        {
+        try {
 
             super.onCreate(savedInstanceState);
 
             AppLoader task = new AppLoader();
             task.execute();
 
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        try
-        {
+        try {
             MenuInflater inflater = getMenuInflater();
 
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
         }
 
         return true;
     }
 
-    private class AppLoader extends AsyncTask<Void, Void, Void>{
+    private class AppLoader extends AsyncTask<Void, Void, Void> {
         ProgressDialog loadingDialog;
         PreferenceScreen root;
 
         protected void onPreExecute() {
-            try
-            {
+            try {
                 CharSequence csMessage;
-                try
-                {
+                try {
                     csMessage = getString(R.string.app_loading);
-                }
-                catch (Exception ex)
-                {
+                } catch (Exception ex) {
                     csMessage = "Loading ...";
                 }
-                loadingDialog = ProgressDialog.show(DroidActivity.this, "",csMessage , true);
-            }
-            catch(Exception ex)
-            {
+                loadingDialog = ProgressDialog.show(DroidActivity.this, "", csMessage, true);
+            } catch (Exception ex) {
             }
         }
 
 
         @Override
         protected Void doInBackground(Void... nothing) {
-            try
-            {
+            try {
                 // Root
                 root = getPreferenceManager().createPreferenceScreen(DroidActivity.this);
 
                 // Inline preferences
-                PreferenceCategory inlinePrefCat = new PreferenceCategory(DroidActivity.this );
+                PreferenceCategory inlinePrefCat = new PreferenceCategory(DroidActivity.this);
                 inlinePrefCat.setTitle(R.string.app_apps);
 
 
@@ -90,40 +77,41 @@ public class DroidActivity extends PreferenceActivity {
                 Collections.sort(packages, new ApplicationInfo.DisplayNameComparator(pm));
 
                 for (ApplicationInfo packageInfo : packages) {
-	                // Checkbox preference
-	            	if (
+                    // Checkbox preference
+                    if (
                             packageInfo.packageName.contentEquals("com.icq.mobile.client") || // ICQ
-	            	        packageInfo.packageName.contentEquals("com.appturbo.appturboBR") || // AppDoDia
-	            	        packageInfo.packageName.contentEquals("com.tencent.mm") || // WeChat
-                            packageInfo.packageName.contentEquals("jp.naver.line.android") || // Line
-                            packageInfo.packageName.contentEquals("com.ebuddy.android") || // eBuddy Messenger
-                            packageInfo.packageName.contentEquals("com.nimbuzz") || // Nimbuzz
-                            packageInfo.packageName.contentEquals("com.imo.android.imoim") || // Imo Messenger
-                            packageInfo.packageName.contentEquals("com.bsb.hike") || // Hike
-	            			packageInfo.packageName.contentEquals("com.sec.chaton") ||
-	            			packageInfo.packageName.contentEquals("com.android.email") ||
-                            packageInfo.packageName.contentEquals("com.google.android.email") || // Emal Nexus 4
-	            			packageInfo.packageName.contentEquals("com.google.android.talk") ||
-	            			packageInfo.packageName.contentEquals("com.facebook.katana") ||
-                            packageInfo.packageName.contentEquals("com.facebook.orca") ||
-                            packageInfo.packageName.contentEquals("com.google.android.gm") ||
-                            //packageInfo.packageName.contentEquals("com.android.vending") ||
-                            packageInfo.packageName.contentEquals("com.google.android.apps.plus") ||
-                            packageInfo.packageName.contentEquals("com.hotmail.Z7") ||
-                            //packageInfo.packageName.contentEquals("com.linkedin.android") ||
-                            packageInfo.packageName.contentEquals("com.android.mms") ||
-                            packageInfo.packageName.contentEquals("com.android.calendar") ||
-                            packageInfo.packageName.contentEquals("com.skype.raider") ||
-                            //packageInfo.packageName.contentEquals("com.android.phone") ||
-                            packageInfo.packageName.contentEquals("com.twitter.android") ||
-                            packageInfo.packageName.contentEquals("com.viber.voip") ||                            
-                            packageInfo.packageName.contentEquals("com.whatsapp") ||
-                            packageInfo.packageName.contentEquals("com.yahoo.mobile.client.android.mail") ||
-                            packageInfo.packageName.contentEquals("com.yahoo.mobile.client.android.im") ||
-                            packageInfo.packageName.contentEquals("com.google.android.apps.orkut"))
+                                    packageInfo.packageName.contentEquals("com.appturbo.appturboBR") || // AppDoDia
+                                    packageInfo.packageName.contentEquals("com.tencent.mm") || // WeChat
+                                    packageInfo.packageName.contentEquals("jp.naver.line.android") || // Line
+                                    packageInfo.packageName.contentEquals("com.ebuddy.android") || // eBuddy Messenger
+                                    packageInfo.packageName.contentEquals("com.nimbuzz") || // Nimbuzz
+                                    packageInfo.packageName.contentEquals("com.imo.android.imoim") || // Imo Messenger
+                                    packageInfo.packageName.contentEquals("com.bsb.hike") || // Hike
+                                    packageInfo.packageName.contentEquals("com.sec.chaton") ||
+                                    packageInfo.packageName.contentEquals("com.android.email") ||
+                                    packageInfo.packageName.contentEquals("com.google.android.email") || // Emal Nexus 4
+                                    packageInfo.packageName.contentEquals("com.google.android.talk") ||
+                                    packageInfo.packageName.contentEquals("com.facebook.katana") ||
+                                    packageInfo.packageName.contentEquals("com.facebook.orca") ||
+                                    packageInfo.packageName.contentEquals("com.google.android.gm") ||
+                                    //packageInfo.packageName.contentEquals("com.android.vending") ||
+                                    packageInfo.packageName.contentEquals("com.google.android.apps.plus") ||
+                                    packageInfo.packageName.contentEquals("com.hotmail.Z7") ||
+                                    //packageInfo.packageName.contentEquals("com.linkedin.android") ||
+                                    packageInfo.packageName.contentEquals("com.android.mms") ||
+                                    packageInfo.packageName.contentEquals("com.android.calendar") ||
+                                    packageInfo.packageName.contentEquals("com.skype.raider") ||
+                                    //packageInfo.packageName.contentEquals("com.android.phone") ||
+                                    packageInfo.packageName.contentEquals("com.twitter.android") ||
+                                    packageInfo.packageName.contentEquals("com.viber.voip") ||
+                                    packageInfo.packageName.contentEquals("com.whatsapp") ||
+                                    packageInfo.packageName.contentEquals("org.telegram.messenger") ||
+                                    packageInfo.packageName.contentEquals("com.yahoo.mobile.client.android.mail") ||
+                                    packageInfo.packageName.contentEquals("com.yahoo.mobile.client.android.im") ||
+                                    packageInfo.packageName.contentEquals("com.google.android.apps.orkut"))
 
                     {
-		                CheckBoxPreference checkboxPref = new CheckBoxPreference(DroidActivity.this);
+                        CheckBoxPreference checkboxPref = new CheckBoxPreference(DroidActivity.this);
                        /*
                         ListPreference lp = new ListPreference(DroidActivity.this);
                         CharSequence[] entries = { "Verde", "Azul", "Amarelo" };
@@ -133,50 +121,37 @@ public class DroidActivity extends PreferenceActivity {
                         */
 
 
-		                if (packageInfo.packageName.contentEquals("com.google.android.talk") && (packageInfo.taskAffinity.equals("android.task.googletalk")))
-                        {
+                        if (packageInfo.packageName.contentEquals("com.google.android.talk") && (packageInfo.taskAffinity.equals("android.task.googletalk"))) {
                             checkboxPref.setKey("com.google.android.gsf"); // GTalk - Estrutura dos servicos do Google
-		                }
-                        else
-                        {
-		                    checkboxPref.setKey(packageInfo.packageName);
+                        } else {
+                            checkboxPref.setKey(packageInfo.packageName);
                         }
 
-                        if (packageInfo.packageName.contentEquals("com.android.mms"))
-                        {
+                        if (packageInfo.packageName.contentEquals("com.android.mms")) {
                             checkboxPref.setTitle("SMS");
-                        }
-                        else if (packageInfo.packageName.contentEquals("com.facebook.orca"))
-                        {
+                        } else if (packageInfo.packageName.contentEquals("com.facebook.orca")) {
                             checkboxPref.setTitle("Facebook Messenger");
-                        }
-                        else
-                        {
-		                    checkboxPref.setTitle( packageInfo.loadLabel(pm));
+                        } else {
+                            checkboxPref.setTitle(packageInfo.loadLabel(pm));
                         }
 
 
-		                inlinePrefCat.addPreference(checkboxPref);
+                        inlinePrefCat.addPreference(checkboxPref);
                         //inlinePrefCat.addPreference(lp);
 
 
-	            	}	
-	            }
-            }
-            catch(Exception ex)
-            {
+                    }
+                }
+            } catch (Exception ex) {
             }
             return null;
         }
 
         protected void onPostExecute(Void nothing) {
-            try
-            {
+            try {
                 setPreferenceScreen(root);
                 loadingDialog.cancel();
-            }
-            catch(Exception ex)
-            {
+            } catch (Exception ex) {
             }
         }
 
